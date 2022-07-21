59d289e4a07c800e4bb2ac43e07f3eef87b7d3c8
Chase <chasedemaster.tech@gmail.com>
2022-07-21 16:32:02 -0500

    commit tree f8f429c8db8ffb8facda0ea4023bf10e8a4385a4

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,31 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+
+
+
+
+
+
+def parrot(pass = "Squawk!")
+  puts "#{pass.to_s}"
+  print "#{pass.to_s}"
+
+end
+
+parrot
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
